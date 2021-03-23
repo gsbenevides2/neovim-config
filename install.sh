8014d0f6de5c8648a330079c7bc580c049a5e6d1
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-03-23 01:23:35 +0000

    add install file

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo 'Set neovim config folder'
+mkdir ~/.config/nvim &> /dev/null
+
+echo 'Cloning comfigs'
+git clone https://github.com/gsbenevides2/neovim-config ~/.config/nvim &> /dev/null
+
+echo 'Install vim plug'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &> /dev/null
+
+echo 'Installngs plugins'
+nvim --headless +PlugInstall +qa &> /dev/null
+
+echo 'Install coc extensions'
+nvim --headless +CocInstall coc-html coc-eslint coc-emmet coc-tsserver coc-styled-components coc-json coc-css +qa
